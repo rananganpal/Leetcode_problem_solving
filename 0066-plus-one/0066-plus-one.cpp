36f940b1569cb3b8c14da0770b9cd07931c38b85
Ranangan Pal <rananaganpal@gmail.com>
2025-09-22 20:14:58 +0530

    Time: 0 ms (100%), Space: 11.5 MB (71.48%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+             int carry = 1;
+        for (int i = digits.size() - 1; i >= 0; --i) {
+            if (carry == 0) break;  
+            int sum = digits[i] + carry;
+            digits[i] = sum % 10;
+            carry = sum / 10;
+        }
+        if (carry) {
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
+    }
+};
