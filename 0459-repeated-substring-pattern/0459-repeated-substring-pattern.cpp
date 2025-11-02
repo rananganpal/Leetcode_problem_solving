3a7b3a9ff4c5bf8e3fb3f45ae355402c86a43b22
Ranangan Pal <rananaganpal@gmail.com>
2025-11-02 18:32:38 +0530

    Time: 3 ms (87.8%), Space: 15.1 MB (80.28%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+    string doubled = s + s;
+    string middle = doubled.substr(1, doubled.size() - 2);
+    return middle.find(s) != string::npos;
+    }
+};
