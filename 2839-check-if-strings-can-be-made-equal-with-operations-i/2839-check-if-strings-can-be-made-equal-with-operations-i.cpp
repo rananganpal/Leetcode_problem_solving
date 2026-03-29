9a583e7f016ac7e8be34dd73715b5e3a9e4f4baf
Ranangan Pal <rananaganpal@gmail.com>
2026-03-29 20:20:26 +0530

    Time: 0 ms (100%), Space: 8.7 MB (32.9%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+       vector<int> even(26, 0),odd(26, 0);
+        for (int i=0;i<s1.size();i++) {
+            if (i%2==0) {
+                even[s1[i]-'a']++;
+                even[s2[i]-'a']--;
+            } else {
+                odd[s1[i]-'a']++;
+                odd[s2[i]-'a']--;
+            }
+        }
+        return even == odd && all_of(even.begin(),even.end(),[](int x){ return x==0; });
+    }
+};
